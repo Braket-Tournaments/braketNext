197407ae2c5dfa9bcc310ea2928c487029947abb
EdisonnTran <80595547+EdisonnTran@users.noreply.github.com>
2022-06-27 10:17:32 -0400

    saving

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,14 +3,16 @@ import styles from "../styles/Home.module.css";
 
 import Navbar from "../components/global/navbar";
 import SupportedGames from "../components/home/supported-games";
+import Features from "../components/home/features";
 
 const Home: NextPage = () => {
   return (
     <>
       <Navbar />
-      {/* <SupportedGames />
       <SupportedGames />
-      <div className={styles.square} /> */}
+      <SupportedGames />
+      <div className={styles.square} />
+      <Features />
     </>
   );
 };
