bf900cb6745eb113cfcca89ee124e4fa0494bb56
Wilson <71456315+wilsonw13@users.noreply.github.com>
2022-07-25 01:21:58 -0400

    made a bit of progress on supported-games

--- components/home/supported-games.tsx
+++ components/home/supported-games.tsx
@@ -2,34 +2,29 @@ import DecorLine from "../items/decor-line";
 
 export default function SupportedGames() {
   function SupportedGameIcon({
-    gameName,
-    initalImg,
-    hoverImg,
+    game,
+    logo,
+    children,
   }: {
-    gameName: string;
-    initalImg: string;
-    hoverImg: string;
+    game: string;
+    logo: string;
+    children: string;
   }) {
     return (
-      <div className="relative w-80%/3 z-1 h-[65%] shadow-2xl hover:cursor-pointer hover:shadow-none">
-        <img
-          className="absolute h-full w-full max-w-full max-h-full transition duration-500"
-          src={hoverImg}
-          alt={gameName}
-        />
-        <img
-          className={
-            "absolute h-full w-full max-w-full max-h-full transition duration-500 group-hover:grayscale-[80%] group-hover:blur-[0.07rem] hover:!scale-110 hover:!opacity-0 z-10"
-          }
-          src={initalImg}
-          alt={gameName}
-        />
+      <div className="relative w-[30rem] h-[30rem] group">
+        <div className="absolute w-full h-full group-hover:opacity-0 duration-[300ms]">
+          <img src={logo} className="w-full h-full" alt={game + " Logo"} />
+        </div>
+        <div className="absolute w-full h-full opacity-0 border-black border-solid border-[0.1rem] flex flex-col align-center justify-center gap-y-[4rem] group-hover:opacity-100 duration-300">
+          <h1 className="text-[3.5rem] text-center">{game}</h1>
+          <p className="text-[1.5rem] px-[2.5rem]">{children}</p>
+        </div>
       </div>
     );
   }
 
   return (
-    <div className="w-full h-[100vh] flex flex-col justify-center gap-y-[25%]">
+    <div className="w-full h-[100vh] flex flex-col justify-evenly">
       <div className="w-[60rem] flex flex-col">
         <p className="mr-[4rem] text-[3.5rem] text-right">SUPPORTED GAMES</p>
         <div className="drop-shadow-arrow-svg flex">
@@ -37,38 +32,22 @@ export default function SupportedGames() {
         </div>
       </div>
 
-      <div className="h-[50%] relative">
-        <div className="flex flex-row h-full w-full mt-1 absolute group">
-          <div className="w-5%"></div>
-          <SupportedGameIcon
-            gameName="Brandon"
-            initalImg="https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Katarina_0.jpg"
-            hoverImg="https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Katarina_1.jpg"
-          />
-          <div className="w-5%"></div>
-          <SupportedGameIcon
-            gameName="Is"
-            initalImg="https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Katarina_2.jpg"
-            hoverImg="https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Katarina_3.jpg"
-          />
-          <div className="w-5%"></div>
-          <SupportedGameIcon
-            gameName="Elo-Deflated"
-            initalImg="https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Katarina_4.jpg"
-            hoverImg="/images/brandon.png"
-          />
-          <div className="w-5%"></div>
-        </div>
-        {/* THIS IS TO MAKE SURE THAT WHEN YOU HOVER THE EDGES OF THE CONTAINER YOU DON'T ALSO GO INTO GRAYSCALE! */}
-        <div className="h-full relative">
-          <div className="w-5%"></div>
-          <div className="relative w-80%/3 -z-10 h-40"></div>
-          <div className="w-5%"></div>
-          <div className="relative w-80%/3 -z-10 h-40"></div>
-          <div className="w-5%"></div>
-          <div className="relative w-80%/3 -z-10 h-40"></div>
-          <div className="w-5%"></div>
-        </div>
+      <div className="w-full h-[60%] flex justify-evenly items-center">
+        <SupportedGameIcon game="Valorant" logo="/images/valorant.png">
+          To be clear, in Valorant, shooting matters. You don’t kill with
+          abilities. Abilities create tactical opportunities to take the right
+          shot. Characters have abilities that augment their gunplay, instead of
+          fighting directly with their abilities.
+        </SupportedGameIcon>
+        <SupportedGameIcon
+          game="League of Legends"
+          logo="/images/leagueoflegends.png"
+        >
+          I hope Zoe wins xD. I’m a Zoe main and she’s just so fun!! People get
+          so trolled by the bubble, and her voice lines are so cute like when
+          she sings about chocolate cake LOL! She’s super random but also
+          smarter than she looks, just like me xD
+        </SupportedGameIcon>
       </div>
     </div>
   );
